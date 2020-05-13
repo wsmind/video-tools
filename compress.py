3772d58d323c5548821684625ae3d1197295f307
Remi Papillie <remi.papillie@gmail.com>
2020-05-13 19:51:14 +0200

    fixed output format for compression

--- compress.py
+++ compress.py
@@ -2,9 +2,9 @@ import os
 import sys
 
 video_file = sys.argv[1]
-output_file = "compressed_" + video_file
+output_file = "compressed_" + os.path.splitext(video_file)[0] + ".mp4"
 
-codec_config = "-codec:v libx264 -crf 18 -bf 2 -flags +cgop -pix_fmt yuv420p -codec:a aac -strict -2 -b:a 384k -movflags faststart"
+codec_config = "-r 25 -s 1920x1080 -codec:v libx264 -crf 18 -bf 2 -flags +cgop -pix_fmt yuv420p -codec:a aac -strict -2 -b:a 384k -movflags faststart"
 ffmpeg_command = "ffmpeg -i \"" + video_file + \
     "\" " + codec_config + " " + output_file
 
